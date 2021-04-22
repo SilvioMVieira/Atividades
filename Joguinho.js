f01066410f08c90277ef18e2bc2eaf017bbd1cbf
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2021-04-22 20:06:03 -0300

    Atualizando arquivos

--- Joguinho.js
+++ Joguinho.js
@@ -8,21 +8,123 @@ vezes foi chutado para acertar
 */
 
 // estrutura de repetição
-var pensei = 21
-var numero = null
-var contTentativas = 0
-do{
-  numero = Number(prompt("digite um numero"));
-  if(numero<pensei){
-    console.log("--------")
-    console.log("é maior que " + numero)
+function ler() {
+  var x = Number(prompt("digite um numero"));
+  return x;
+}
+
+var jogadores = [];
+var pensei = Number(prompt("Pense num número"));
+var numero = null;
+var contTentativas = 0;
+var player = { nome: "", pontuacao: 0 };
+
+function joga() {
+  player.nome = prompt("digite o nome do jogador");
+
+  do {
+    contTentativas = contTentativas + 1;
+    numero = ler();
+
+    if (numero < pensei) {
+      console.log("--------");
+      console.log(player.nome + " é maior que " + numero);
+    }
+    if (numero > pensei) {
+      console.log("--------");
+      console.log(player.nome + " é menor que " + numero);
+    }
+    if (numero === pensei) {
+      console.log(player.nome + " acertô mizeravi! ");
+      console.log("Numero de tentativas " + contTentativas);
+
+      player.pontuacao =
+        contTentativas < player.pontuacao || player.pontuacao === 0
+          ? contTentativas
+          : player.pontuacao;
+
+      jogadores.push(player);
+    }
+  } while (numero !== pensei);
+  return console.log(jogadores);
+}
+console.log(jogadores);
+var contin = prompt("Deseja continuar?")
+if (contin == "Sim") {
+  function joga()
+}
+
+
+//Outra forma
+function ler() {
+  var x = Number(prompt("Digite um número qualquer " + player.nome + "."));
+  return x;
+}
+var jogadores = [];
+var ranking = [];
+var contTentativas = 0;
+
+function joga() {
+  do {
+    var numero = null;
+    contTentativas = contTentativas + 1;
+    numero = ler();
+    if (numero < pensei) {
+      alert("Errou! O número que você quer advinhar é maior que " + numero + ".\n" + player.nome + " tente novamente.");
+    }
+    if (numero > pensei) {
+      alert("Errou! O número que você quer advinhar é menor que " + numero + ".\n" + player.nome + ". Tente novamente.");
+    }
+    if (numero === pensei) {
+      alert(player.nome + " , você acertou o número em " + contTentativas + " tentativas!");
+
+      player.pontuacao =
+        contTentativas < player.pontuacao || player.pontuacao === 0
+          ? contTentativas
+          : player.pontuacao;
+
+      jogadores.push(player);
+    }
+  } while (numero !== pensei);
+  return player;
+}
+alert("Bem vindo ao jogo 'Advinhe o Número!' \n\n Advinhe o número aleatório com menos tentativas, \n e genhe de seus adversários")
+do {
+  var opcao = Number(prompt("Informe a opção desejada: \n 1 - Novo jogo \n 2 - Sair do jogo"))
+  var numJogadores = Number(prompt("Informe a quantidade de jogadores"))
+  for (var i = 0; i < numJogadores; i++) {
+    if (opcao == 1) {
+      var player = { nome: "", pontuacao: 0 };
+      player.nome = prompt("Digite o nome do jogador");
+      var pensei = Math.floor(Math.random() * 100);
+      joga()
+    }
+    if (opcao == 2) {
+      close
+    }
+    contTentativas = 0
   }
-  if(numero>pensei){
-    console.log("--------")
-    console.log("é menor que " + numero)
+  var continua = Number(prompt("Digite 1 para entrar no jogo novamente \nDigite 0 para sair "))
+} while (continua == 1)
+
+if (continua == 1) {
+  var contTentativas = 0
+  pensei = Math.floor(Math.random() * 100);
+  joga()
+}
+if (continua == 2) {
+  close
+}
+for (var i = 0; i < jogadores.length; i++) {
+  for (var j = i + 1; j < jogadores.length; j++) {
+    if (jogadores[i].pontuacao > jogadores[j].pontuacao) {
+      var aux = jogadores[i]
+      jogadores[i] = jogadores[j]
+      jogadores[j] = aux;
+    }
   }
-  if(numero === pensei){
-    console.log("Acertô mizeravi! ")
-  }
-  // contTentativas = contTentativas + 1;
-}while (numero !== pensei);
+  alert("Parabéns " + jogadores[0].nome + ", você ganhou o jogo com apenas " + jogadores[0].pontuacao + " tentativas!")
+  console.log("Ranking")
+  jogadores
+}
+
